0478d7432c4b83bef299618354b678fc822f846b
ZmiyevskiyVladislav <zmiievskiy@gmail.com>
2022-05-28 00:20:43 +0300

    created class Counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {Counter} from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Counter  value={0} min={-5} max={10}/>
+        <Counter value={5} min={0} max={10}/>
+        <Counter value={5} min={0} />
         <a
           className="App-link"
           href="https://reactjs.org"
