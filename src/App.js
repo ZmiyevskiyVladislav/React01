250813954c91baa7c867afe17519ee698716c480
ZmiyevskiyVladislav <zmiievskiy@gmail.com>
2022-05-29 18:29:25 +0300

    Created counter with using functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import {Counter} from "./components/Counter";
+import React, {useState} from 'react'
+function computeInitialCounter() {
+  console.log('Some calculation');
+  return Math.trunc(Math.random() * 20)
+}
 
 function App() {
+  const [counter, setCounter]= useState(0);
+  const [counter2, setCounter2]= useState(() => {
+    return computeInitialCounter()
+  });
+
+  function increment() {
+    setCounter(counter + 1)
+    setCounter2((prevCounter) => {
+      return prevCounter + 1
+    })
+  }
+  function decrement() {
+    setCounter(counter - 1);
+    setCounter2((prevCounter) => {
+      return prevCounter - 1
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter  value={0} min={-5} max={10}/>
-        <Counter value={5} min={0} max={10}/>
-        <Counter value={5} min={0} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Counter: {counter}</h2>
+            <div>
+              <button onClick={increment} className="increment">+</button>
+              <button onClick={decrement} className="decrement">-</button>
+            </div>
+            <h2>Counter: {counter2}</h2>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </>
   );
 }
 
